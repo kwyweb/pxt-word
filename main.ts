25c4a38689d88ff8868409961364a17645a393bc
kwy <41284741+kwyweb@users.noreply.github.com>
2020-04-05 20:54:45 +0800

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,18 @@
+//% weight=0 color=#3CB371 icon="\uf6af" block="Word "
+namespace word {
+    /**
+    * Create text content to display on scree.
+    */
+    //% blockId="createTextSprite" block="Display text of Text %text|font-color %fg|background-color %bg"
+    //% blockGap=2 weight=0 blockExternalInputs=true
+    export function createTextSprite(text: string, fg: number, bg: number){
+	const font = image.font8;
+	const width = font.charWidth * text.length;
+	const height = font.charHeight;
+	const res = image.create(width, height);
+	res.fill(bg);
+	res.print(text, 0, 0, fg, font);
+	const sprite = sprites.create(res, SpriteKind.Food);
+	return sprite;	
+    }
+}
